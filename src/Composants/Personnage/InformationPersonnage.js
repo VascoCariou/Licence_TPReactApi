e7e2bdf04845faf899c65d818789cd7a61a7b45b
E22A464X <vasco.cariou@etu.univ-nantes.fr>
2022-11-25 09:01:32 +0100

    Test push vercel

--- src/Composants/Personnage/InformationPersonnage.js
+++ src/Composants/Personnage/InformationPersonnage.js
@@ -79,6 +79,9 @@ const InformationPersonnage = (props) => {
           />
         </ul>
       </li>
+      <li>
+        AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+      </li>
     </div>
   );
 };
